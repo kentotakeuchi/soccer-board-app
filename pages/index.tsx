a60454dea162df46a75707e5d7afc1e05a5c0917
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-08-15 23:39:43 -0700

    wip: hooking up apollo-client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,54 @@
 import Head from 'next/head'
 import Field from '../components/Field'
+import { useQuery, gql } from '@apollo/client'
 
-export default function Home() {
+const GET_DOGS = gql`
+  query GetDogs {
+    dogs {
+      id
+      breed
+    }
+  }
+`
+
+const GET_DOG_PHOTO = gql`
+  query Dog($breed: String!) {
+    dog(breed: $breed) {
+      id
+      displayImage
+    }
+  }
+`
+
+function Dogs({ onDogSelected }: any) {
+  const { loading, error, data } = useQuery(GET_DOGS)
+
+  if (loading) return 'Loading...'
+  if (error) return `Error! ${error.message}`
+
+  return (
+    <select name='dog' onChange={onDogSelected}>
+      {data.dogs.map((dog: any) => (
+        <option key={dog.id} value={dog.breed}>
+          {dog.breed}
+        </option>
+      ))}
+    </select>
+  )
+}
+
+function DogPhoto({ breed }: any) {
+  const { loading, error, data } = useQuery(GET_DOG_PHOTO, {
+    variables: { breed }
+  })
+
+  if (loading) return null
+  if (error) return `Error! ${error}`
+
+  return <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
+}
+
+export default function HomePage() {
   return (
     <>
       <Head>
@@ -10,8 +57,9 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <p>home</p>
       <Field />
+      <p>home</p>
+      <Dogs />
     </>
   )
 }
