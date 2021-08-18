ccc80089f7bdd6c8aba7b2f9f100bb009b6df1be
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-08-18 00:51:42 -0700

    wip: apollo-server

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+
+  // Webpack module bundler used by Next.js doesn’t know how to bundle schema.graphql file; to fix this we can use package webpack-graphql-loader
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.graphql?$/,
+      loader: 'webpack-graphql-loader'
+    })
+
+    return config
+  }
 }
