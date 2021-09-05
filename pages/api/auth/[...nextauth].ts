b439caf655ad0b30a35d12f01be8c905db84f272
kentotakeuchi <ideal.kt.reality@gmail.com>
2021-09-04 22:32:35 -0700

    wip: debugging next-auth..

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
-import NextAuth from 'next-auth'
+import NextAuth, { User as NextAuthUser } from 'next-auth'
 import Providers from 'next-auth/providers'
 import Adapters from 'next-auth/adapters'
 import Models from '../../../lib/db/models'
+interface NextAuthUserWithStringId extends NextAuthUser {
+  id: string
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -23,10 +26,33 @@ export default NextAuth({
     }),
     Providers.GitHub({
       clientId: process.env.NEXTAUTH_GITHUB_CLIENT_ID,
-      clientSecret: process.env.NEXTAUTH_GITHUB_CLIENT_SECRET
+      clientSecret: process.env.NEXTAUTH_GITHUB_CLIENT_SECRET,
+      profile(profile: any) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url
+        } as NextAuthUserWithStringId
+      }
     })
     // ...add more providers here
   ],
+
+  session: {
+    // Use JSON Web Tokens for session instead of database sessions.
+    // This option can be used with or without a database for users/accounts.
+    // Note: `jwt` is automatically set to `true` if no database is specified.
+    jwt: true,
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    updateAge: 24 * 60 * 60 // 24 hours
+  },
 
   // todo: insert custom field in user model
   // // Extend the built-in models
